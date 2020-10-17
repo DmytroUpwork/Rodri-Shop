6b378fa2ec9ff3e997440990cc817db9dd89795f
Dmitrii <lomonovskij@gmail.com>
2020-10-17 19:10:48 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+  // Настройки слайдера диапазона цен ===============
+  $(function () {
+    $("#slider-range").slider({
+      range: true,
+      min: 1,
+      max: 100,
+      values: [10, 80],
+      slide: function (event, ui) {
+        $("#price").val(ui.values[0] + " ₽     " + +ui.values[1] + " ₽ ");
+      },
+    });
+    $("#price").val(
+      $("#slider-range").slider("values", 0) +
+        " ₽     " +
+        $("#slider-range").slider("values", 1) +
+        " ₽"
+    );
+  });
+
+
+  // Поле текущей даты в форме =====================
+  $("#myDate").val(new Date().toISOString().slice(0, 10));
+
+  $(".wrapper").mousewheel(function (e, delta) {
+    this.scrollLeft -= delta * 40;
+    e.preventDefault();
+  });
+  
+  
+});
+
+
+
