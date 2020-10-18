17a558ce1fef969605f9d857921aa0b8611a9fc3
Dmitrii <lomonovskij@gmail.com>
2020-10-18 14:59:47 +0300

    commit

---
+++ libs/dashboard-analytics/dashboard-ecommerce.js
@@ -0,0 +1,123 @@
+/*=========================================================================================
+    File Name: dashboard-ecommerce.js
+    Description: dashboard ecommerce page content with Apexchart Examples
+    ----------------------------------------------------------------------------------------
+    Item Name: Vuexy  - Vuejs, HTML & Laravel Admin Dashboard Template
+    Author: PIXINVENT
+    Author URL: http://www.themeforest.net/user/pixinvent
+==========================================================================================*/
+
+$(window).on("load", function () {
+
+  var $primary = '#7367F0';
+  var $success = '#28C76F';
+  var $danger = '#EA5455';
+  var $warning = '#FF9F43';
+  var $info = '#00cfe8';
+  var $primary_light = '#A9A2F6';
+  var $danger_light = '#f29292';
+  var $success_light = '#55DD92';
+  var $warning_light = '#ffc085';
+  var $info_light = '#1fcadb';
+  var $strok_color = '#b9c3cd';
+  var $label_color = '#e7e7e7';
+  var $white = '#fff';
+  
+  
+  // Session Chart
+  // ----------------------------------
+
+  var sessionChartoptions = {
+    chart: {
+      type: 'donut',
+      height: 325,
+      toolbar: {
+        show: false
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    series: [58.6, 34.9, 6.5],
+    legend: { show: false },
+    comparedResult: [2, -3, 8],
+    labels: ['Товар 1', 'Товар 2', 'Товар 3'],
+    stroke: { width: 0 },
+    colors: [$primary, $warning, $danger],
+    fill: {
+      type: 'gradient',
+      gradient: {
+        gradientToColors: [$primary_light, $warning_light, $danger_light]
+      }
+    }
+  }
+
+  var sessionChart = new ApexCharts(
+    document.querySelector("#statistics-chart"),
+    sessionChartoptions
+  );
+
+  sessionChart.render();
+
+
+
+
+
+  
+
+  // Customer Chart
+  // -----------------------------
+
+  var customerChartoptions = {
+    chart: {
+      type: 'pie',
+      height: 330,
+      dropShadow: {
+        enabled: false,
+        blur: 5,
+        left: 1,
+        top: 1,
+        opacity: 0.2
+      },
+      toolbar: {
+        show: false
+      }
+    },
+    labels: ['New', 'Returning', 'Referrals'],
+    series: [690, 258, 149],
+    dataLabels: {
+      enabled: false
+    },
+    legend: { show: false },
+    stroke: {
+      width: 5
+    },
+    colors: [$primary, $warning, $danger],
+    fill: {
+      type: 'gradient',
+      gradient: {
+        gradientToColors: [$primary_light, $warning_light, $danger_light]
+      }
+    }
+  }
+
+  var customerChart = new ApexCharts(
+    document.querySelector("#customer-chart"),
+    customerChartoptions
+  );
+
+  customerChart.render();
+
+});
+
+
+// Chat Application
+(function ($) {
+  "use strict";
+  // Chat area
+  if ($('.chat-application .user-chats').length > 0) {
+    var chat_user = new PerfectScrollbar(".user-chats", { wheelPropagation: false });
+  }
+
+})(jQuery);
+
